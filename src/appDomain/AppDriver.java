5799ebe9e9d91ec53f4f7ce8184ba6ed5ab5c40e
yufeng-fan <yufeng.fan@edu.sait.ca>
2026-02-18 18:37:06 -0700

    Initial commit

---
+++ src/appDomain/AppDriver.java
@@ -0,0 +1,37 @@
+package appDomain;
+
+/**
+ * <p>
+ * This application driver code is designed to be used as a basis for the
+ * Complexity and Sorting assignment that will be developed in the CPRG304 
+ * W2026 class at SAIT. The implementors of this applications will be required
+ * to add all the correct functionality.
+ * </p>
+ */
+public class AppDriver
+{
+	/**
+	 *  The main method is the entry point of the application.
+	 *  
+	 *  @param args The input to control the execution of the application.
+	 */
+	public static void main( String[] args )
+	{
+		// TODO Auto-generated method stub
+
+		// refer to demo00 BasicFileIO.java for a simple example on how to
+		// read data from a text file
+
+		// refer to demo01 Test.java for an example on how to parse command
+		// line arguments and benchmarking tests
+
+		// refer to demo02 Student.java for comparable implementation, and
+		// NameCompare.java or GradeCompare for comparator implementations
+
+		// refer to demo02 KittySort.java on how to use a custom sorting
+		// algorithm on a list of comparables to sort using either the
+		// natural order (comparable) or other orders (comparators)
+
+	}
+
+}
