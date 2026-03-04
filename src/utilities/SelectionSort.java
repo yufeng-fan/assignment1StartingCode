4cae9fe25aac67a839cec31e4e64c0c6e3e7f5f7
JianningLi-Richard <156393370+JianningLi-Richard@users.noreply.github.com>
2026-03-03 18:52:25 -0700

    selection sort

--- src/utilities/SelectionSort.java
+++ src/utilities/SelectionSort.java
@@ -9,13 +9,40 @@ public class SelectionSort implements SortStrategy {
 	@Override
 	public void sort(Comparable<Shape>[] array) {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < array.length - 1; i++) {
 
+	        int maxIndex = i;
+
+	        for (int j = i + 1; j < array.length; j++) {
+	            if (array[j].compareTo((Shape) array[maxIndex]) > 0) {
+	                maxIndex = j;
+	            }
+	        }
+
+	        Comparable<Shape> temp = array[i];
+	        array[i] = array[maxIndex];
+	        array[maxIndex] = temp;
+	    }
 	}
 
 	@Override
 	public void sort(Comparable<Shape>[] array, Comparator<? super Shape> comp) {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < array.length - 1; i++) {
 
+	        int maxIndex = i;
+
+	        for (int j = i + 1; j < array.length; j++) {
+	            if (comp.compare((Shape) array[j],
+	                             (Shape) array[maxIndex]) > 0) {
+	                maxIndex = j;
+	            }
+	        }
+
+	        Comparable<Shape> temp = array[i];
+	        array[i] = array[maxIndex];
+	        array[maxIndex] = temp;
+	    }
 	}
 
 }
